7405bfc0500e711dace259994f0f5b9122f13464
Quinton Fults <quinton.fults.work@gmail.com>
2022-12-13 13:27:29 -0800

    code

--- server.js
+++ server.js
@@ -1,30 +1,50 @@
-const express = require('express')
-const mysql = require('mysql2')
+const express = require("express");
+const mysql = require("mysql2");
 
-const app = express()
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'rootroot',
-  database: 'users_db'
-})
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  database: "users_db",
+});
 
-db.query('SELECT * FROM users', (err, users) => {
-  if (err) throw err
-  console.log(users)
-})
+app.get("/api/users", (req, res) => {
+  db.query("SELECT * FROM users", (err, users) => {
+    if (err) throw err;
+    res.json(users);
+  });
+});
 
-const user = {
-  username: 'johndoe',
-  age: 21
-}
+// const user = {
+//   username: 'johndoe',
+//   age: 21
+// }
 
-// db.query('INSERT INTO users SET ?', user, (err) => {
+app.post("/api/users", (req, res) => {
+  db.query("INSERT INTO users SET ?", req.body, (err) => {
+    if (err) throw err;
+    res.sendStatus(200)
+  });
+});
+
+// const condition = { username: 'janedoe' }
+// const updates = { age: 25 }
+
+// db.query('UPDATE users SET ? WHERE ?', [updates, condition], (err) => {
 //   if (err) throw err
-//   console.log('User added!')
+//   console.log('Data modified')
 // })
 
+// const condition = { username: 'johndoe' }
 
+// db.query('DELETE FROM users WHERE ?', condition, (err) => {
+//   if (err) throw err
+//   console.log('Data deleted')
+// })
 
-app.listen(3001)
+app.listen(3001);
